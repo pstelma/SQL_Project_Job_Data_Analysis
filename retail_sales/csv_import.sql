f04f3ecaa639cdea7efd88a4d55fbbb6377efb42
pstelma <pstelmaszczyk00@gmail.com>
2025-03-23 19:21:21 +0100

    removed unneccesary files

--- retail_sales/csv_import.sql
+++
@@ -1,3 +0,0 @@
-COPY products FROM 'C:\Users\lukke\Desktop\sql_project_2\products.csv' DELIMITER ',' CSV HEADER;
-COPY customers FROM 'C:\Users\lukke\Desktop\sql_project_2\customers.csv' DELIMITER ',' CSV HEADER;
-COPY sales FROM 'C:\Users\lukke\Desktop\sql_project_2\sales.csv' DELIMITER ',' CSV HEADER;
